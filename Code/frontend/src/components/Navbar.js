682b02abe95435b3c69464fb79c02007d676aeca
Anuraag Jajoo <anuraagjajoo@gmail.com>
2024-11-01 19:18:54 -0400

    Added doco for Meal Plan List and Recipe component

--- Code/frontend/src/components/Navbar.js
+++ Code/frontend/src/components/Navbar.js
@@ -1,3 +1,41 @@
+/**
+ * Nav Component
+ *
+ * This component renders a navigation bar for the application, including a logo,
+ * a welcome message for the user, and options to access bookmarks, meal plans, or log out.
+ * It is designed to adjust its appearance based on the user's color mode preference.
+ *
+ * Features:
+ * - Displays a logo and application title ("Saveurs Sélection").
+ * - Shows a welcome message to the logged-in user with options to navigate to
+ *   bookmarks and meal plans, or to log out.
+ * - If no user is logged in, prompts the user to log in to access their favorite recipes.
+ * - Uses Chakra UI components for consistent styling and responsiveness.
+ *
+ * State Management:
+ * - This component does not maintain its own state but receives props to manage user interactions
+ *   and display information.
+ *
+ * Functions:
+ * - `handleBookMarks()`: Calls the `handleBookMarks` prop function to navigate to the user's bookmarks.
+ * - `handleMealPlan()`: Calls the `handleMealPlan` prop function to navigate to the user's meal plan.
+ * - `handleLogout()`: Logs the user out by calling the `handleLogout` prop function and logs a message to the console.
+ *
+ * Props:
+ * - user (object): An object representing the logged-in user, including properties such as `userName`.
+ * - handleBookMarks (function): A callback function to handle navigating to the bookmarks section.
+ * - handleMealPlan (function): A callback function to handle navigating to the meal plan section.
+ * - handleLogout (function): A callback function to handle user logout.
+ *
+ * Rendering:
+ * - The component renders a navigation bar with a logo, a heading, and a menu for user options.
+ *   The menu displays when a user is logged in, showing the user's name and options for bookmarks, meal plans, and logout.
+ * - If the user is not logged in, a prompt to log in is displayed.
+ *
+ * @returns {JSX.Element} The rendered Nav component, providing a navigation interface
+ *                        for the application.
+ */
+
 "use client";
 
 import {
