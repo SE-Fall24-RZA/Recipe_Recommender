93182bc223ee72f0a5f70651007742fa6817c6d5
Anuraag Jajoo <anuraagjajoo@gmail.com>
2024-11-01 16:43:16 -0400

    Added doco for RecipeList and Search

--- Code/frontend/src/components/SearchByRecipe.js
+++ Code/frontend/src/components/SearchByRecipe.js
@@ -1,3 +1,28 @@
+/**
+ * SearchByRecipe Component
+ *
+ * A search input field that allows users to search for recipes by entering a recipe name.
+ * When the user clicks the "Search" button, the search term is passed to the parent component
+ * via a callback function.
+ *
+ * Props:
+ * @param {Function} sendRecipeData - Callback function that sends the entered recipe name to the parent component.
+ *
+ * State:
+ * @property {string} recipeName - Stores the current value of the recipe name input field.
+ *
+ * Functions:
+ * handleNameChange - Updates the recipeName state with the input field's current value.
+ * handleSearchByRecipeClick - Calls the sendRecipeData callback with the entered recipe name.
+ *
+ * UI Elements:
+ * - An input field for entering a recipe name.
+ * - A search button that triggers the search when clicked.
+ *
+ * Example Usage:
+ * <SearchByRecipe sendRecipeData={handleSearch} />
+ */
+
 /* MIT License
 
 Copyright (c) 2023 Pannaga Rao, Harshitha, Prathima, Karthik  */
